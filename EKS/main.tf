001edb058a9f5ca3f39d20fcf844c24973c54769
Artem Kolomiichuk <111369267+artbart833@users.noreply.github.com>
2024-12-15 21:41:40 +0200

    change

--- EKS/main.tf
+++ EKS/main.tf
@@ -55,10 +55,7 @@ module "eks" {
 
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-  depends_on = [
-    module.eks
-  ]
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {

2dca332f0179a202d9a98e65db4bc3cb4f76c797
Artem Kolomiichuk <111369267+artbart833@users.noreply.github.com>
2024-12-15 19:02:56 +0200

    change main.tf file

--- EKS/main.tf
+++ EKS/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
 
   azs                  = data.aws_availability_zones.azs.names
   public_subnets       = var.public_subnets
-  private_subnets      = var.public_subnets
+  private_subnets      = var.private_subnets
   enable_dns_hostnames = true
 
   enable_nat_gateway = true
